f794c40af6d20dc6cef8c71dc6d149ae311f54a4
Simo Virokannas <simo@virokannas.fi>
2024-04-29 23:03:42 -0400

    Weasels

---
+++ README.md
@@ -0,0 +1,14 @@
+# Weasel VGA
+
+## I'm sorry... what?
+
+The weasels like the standard VGA palette. They would like to communicate its contents, but despite many attempts
+this has been interpreted as series of loud and aggressive hissing noises.
+
+This respository provides an example excerpt of a dialogue between two weasels and a decoder to interpret it as
+the standard VGA palette as a series of 8-bit integers, 0 - 255. The palette consists of 256 colors so the total
+length is 768.
+
+## Why?
+
+The weasels are not at liberty to divulge this information at this time.
---
+++ hiss_vga.py
@@ -0,0 +1,290 @@
+hisses = """Hiss Hihs hssh Hssh Hsss hs Hihhs sh Hshsh hiss SS Hshhh ss hshss Hshs Hiss ss Hsss!
+sh Hsshh sh Hsshs hs Hshhh HS hsshh HS Hshhh hihh Hsss SH hihss sh hshss Hish ss hiss.
+ss Hsshs SH Hissh sh Hshhs s s hsss ss hishh sh hsshs ss hsssh ss Hshss hs hsshh HS Hiss!
+S s s s S S s S s S hih Hishs hs.
+Hishh s S his hihss hss Hihsh s S S S hih Hihs.
+Hsh hihhh Hsh Hshhs hs!
+hihsh hih hsss.
+Hs.
+hsshh s S s s hsh hissh hi.
+Hsssh S S hih hihss hih hihhs hs!
+hihhh his hishs his hshh.
+hi.
+Hiss!
+s s hi.
+hshhs Hi.
+hihsh hsh hshhs his hshss Hss hihhh hsh Hishs hshh Hsss Hih.
+hshhshs.
+hissh Hihshhhs Hshhs Hsssshsh Hssss Hsssshh hisssshhh hsssshs Hishhshhh Hsshshs hihhhssss hishhh!
+Hsh sh Hihsshh Hsh hs hshhhhs Hsh sh hishsh hs Hs.
+Hsshhh SH Hih hihhhh ss hss hshh!
+s Hihh.
+s hshhh s S Hsshhhss s hihssshh s hsshhshh SH s Hshsshsh ss S Hihshhh!
+hs s hssshhsh SS hs Hsshhhhs ss HS hisshssh hs SH hihhhhhh sh his Hshhshs sh Hss Hsssss!
+HS Hih hsshhsh hs Hshs Hssshh.
+SH Hihs Hsshhsh SS Hshs Hihshsh SH hishh hisshhh sh Hshhh hihhssh sh hishh hihshs.
+Hs!
+s HS S hsh S sh s Hss S sh s hss ss His s His sh hsh S hih ss His s Hih hsss hihhs!
+Hsh Hssh Hihssh Hih hssh Hssshs hss hssh!
+hsh Hssss Hi.
+Hisss Hih Hsshh Hss Hihhh Hih Hsshs hssh Hsss hsh hssss his hihhs Hshs.
+Hisshhhh Hihh hss hihhs hsh hsshh ss sh His Hss Hihs Hss Hssss Hih hsssh Hss s Hs.
+hshh Hsh!
+Hss hsss.
+hih hshsh Hs.
+hshh.
+hss Hishs hshh his hsssh his Hihhs Hsh hsshh his hihhs Hshh hss S Hi.
+hshh His hshhh Hih hshhh hshs ss ss Hsh Hsh His Hishs his hsssh hihs Hsss.
+hihhsshs hi.
+Hsshs Hsh Hisss hiss hihh Hsh Hshhs Hih Hshss hihss Hshhhhsh hsss hi.
+hihhh Hss Hihhs HS hs Hsh His Hshs His Hsss!
+Hih hshs.
+hsh s His Hihh Hss!
+Hsh Hshsh Hih hshh!
+hs!
+hihhh His hshss Hih.
+His s hih Hshs his Hshss hsh Hihss hsss SS hs Hs!
+hss Hs!
+hshss Hih hssss Hiss hshss hisssss!
+hsh hssh!
+Hih Hshs.
+hshs hish hs!
+Hshss Hss hshhh hihs hshs His Hishs his Hshhs Hsshh hihhhshs Hiss Hi!
+Hsshh hih Hssss hs hs hsh hsh hsh!
+hs.
+hssh!
+hss Hssss Hsh S hih hssh hiss Hsh Hshhs hss hsssh Hih hsssh His hiss!
+Hihs Hih S Hih Hssh hih Hishh Hih hihhh Hshh sh hs Hss his Hss hishh hi.
+hish.
+Hish hishh hishhssh hss hshhh Hs!
+Hishs hs hs hsh Hsh HS hs Hsh His Hsh Hihss His hsshs sh hshs hsh hiss HS SH Hi!
+Hss Hsh hshss Hih hshsh Hsh s hss hih!
+SH SS his His Hi.
+hsshs Hss hihhs hss Hih Hsh Hsshs hs ss hih hs.
+hs!
+hshsh Hs.
+Hishs Hss hsshs hi!
+Hsshs HS ss His his Hsh Hsssh hi.
+Hsssh Hih hishh his Hsshs sh SS Hss Hih Hsh hss Hss hssss hsh hshsh hss Hssss hs ss hi.
+hsh hih s his His!
+Hi!
+hissh Hss Hshss SH sh his hs!
+SH hish Hss hihs His Hsss.
+hss hihh.
+SS ss Hss hih HS SS Hih Hih hss hishh hih hishh SH Hihs hsh hihh ss sh hsh hi!
+hi.
+hsshh Hsh hshss hih S hs.
+hihs HS sh hsh hs!
+Hsh hihhs Hs.
+hishs His Hsh his hshsh HS ss Hss hih hs!
+Hihss hi.
+hihss His hihss his hshs!
+sh ss Hih hih Hss hss Hsh Hssss hss hissh Hsh hihhh hs hs his his His s his Hssh hih Hshh.
+His hisss SS HS hs!
+hsh hs Hihh Hih hish Hih Hihsh Hih Hihss HS SS his Hsh SH sh Hih his His Hsssh Hsh hsss!
+HS SH Hi!
+Hsh HS hs Hih his His hish.
+His Hshhh SS hsss hsh His!
+sh hs hih hih hih hshsh Hss Hishs Hsh S Hsh hshh SS SS hss Hih hsh Hshss hss Hihs!
+Hsh Hih Hss Hihss SS hs his hss Hih Hshh.
+hsh Hissh Hss hihsh Hsh hihh.
+hs sh hss His Hs.
+his Hsh hssss His Hihhh his Hshhs sh SH hih Hih hsh s hih hssh Hi!
+Hsshs Hss Hihss sh sh Hsh hsh ss hshh hi!
+Hshh hss hihsh hi.
+Hshhh Hih S hs s hih s ss S hsh hsssh Hsh Hishh Hsh ss hs SS Hs!
+s SH S hih Hshs!
+Hs.
+Hishs Hsh hih his S hss S HS s hi.
+hihss hs!
+hissh His hiss Hi.
+S hss s SS S hi!
+hshsh His Hshsh hsh Hisss hss hihsh hsh s SS S His Hihhs hss hisss Hsh Hsshh hih hsss.
+hih S ss s His Hish his s hs!
+hihhh His hihs.
+His s ss S his hsh Hss s hss Hshhh hss Hihsh hss S sh S his hs SS hs Hss hsssh hsh Hshsh hsh s HS S Hs.
+s sh s hsh Hihhh hi.
+Hshhs Hss sh SH SH hi!
+s sh S His hshs!
+Hs.
+hsshh Hih hsh hi!
+s hss S HS S his hshsh Hsh hshhs hss Hish hih S hsh s SS S Hih Hssh.
+hih hishs Hih Hsshh His Hissh hih S sh S his His.
+Hih S hih hshhh His Hsshs Hih s HS s Hss hi.
+Hsh S His hshss His hshss Hih s SH s hss hs HS SS Hi!
+Hishs Hsh hihsh Hih S HS s his s ss S His hsshs Hi.
+hishs hs.
+s sh S his S sh s His Hshhh His hihss His HS HS SS hih s hs S hsh Hssh!
+Hs.
+hihss his His his s Hih s SH s hss hsshh Hsh hihss his Hssh hsh S hsh S hs s Hsh Hishh Hsh Hshss Hss hshhh Hsh hiss!
+hih s hs S Hih hshs hih s His Hishs his Hssh.
+Hsh s sh S hss hsh Hss s his Hshhs hih hihhs hih s SH s Hi.
+HS ss hs hs!
+hshhs Hss hshsh Hih.
+hiss hs sh hshshhhs Hihhshss hsh hs hssh SS SS Hihhhsss hshhss sh Hih Hish ss hs Hsssssss s s Hiss hs SH Hsssshss ss sh hsshsshs Hshs ss SH hisshhs.
+HS SH hihhshh.
+hihs S S hs SS Hshhhshh Hish Hshsss HS hih SH hs hsshshsh Hshh Hshhhhsh Hi.
+SS hs ss hshshhs!
+hsh.
+hiss hs HS Hsssshhs hihshhsh Hsh SS hsss SH HS Hisshshs hshhss sh His hihh HS hs hsshhhs!
+S S Hshh HS ss hihshshh hs sh Hihshshs Hish S S HS HS Hsshshss hsss Hsshhs sh hss HS sh Hshshshs hssh hssssssh his hs SH ss hishshh!
+Hish hsss sh hs hsssshss hssh Hssh SS sh Hisssshs Hihssshs Hih ss hshh ss ss Hihshhsh hshhss sh Hsh Hiss SH SH Hshhhshs S s hih.
+SH hs hihhshs!
+HS SS Hihsssss Hiss S s SS SS Hihssshs Hiss Hssshh SH Hsh ss HS Hsshshh.
+hiss Hssshhs.
+Hsh ss HS HS Hissssss hihssh SS hih hihshh HS His ss ss hissshhh hisss s Hshssh hs hsh sh HS hshsshsh s s Hshhsh HS hsh hs sh hisshshs hs s hshhhss.
+Hihsss HS hsh HS sh Hssshhhh HS SH hishhshs Hihssh SH hi.
+SS SS Hshsshhs SH SS Hsssssss hsshhs HS hsh SH S hshhshss hs ss Hishhshh Hishs!
+ss Hss s S SH HS hshsshsh Hssshh hs hi.
+Hish!
+S SH SS Hihhhssh Hsshhs hs hih hisss.
+sh his HS sh hshshshs Hshhh S hssssh SH His hs hs Hihshsss s S hsshsh SS Hss sh SS Hihhhshs SS s Hsshhshs hihhss SH Hih SS sh hihssssh SS ss Hsssshss Hsshsh ss hsh SS S Hihssss!
+SS sh Hsshssss Hishs!
+hs hi.
+S S HS ss hshsshhs Hishsh hs Hih hishs s hs sh Hihhhhhh Hssss!
+ss Hss hisshh sh his HS hs hssshhhs hsshsh sh His Hshsss SS his hs HS Hshhhhsh hisss S hishsh sh Hsh ss HS hihshhhh S S Hssshs SS hsh SH SH Hssshhs!
+sh s hishsshs hihhh!
+SH hs.
+SS hs Hissshss ss HS Hsshshhs Hishs.
+SH hi!
+HS s Hsshhhhs hs hs Hihshhhh hshsss ss Hsh s S SH sh hsshshsh hshsss hs hss hshss s SS HS Hisshhs.
+S Hihhshhh s Hssshhh.
+ss HS hishhss.
+S sh Hihh s Hihsshhh SS hs Hssssssh SS s hisshshs S hihhhhs.
+sh SS hshshsh.
+ss S HS his.
+S hihshhhs ss ss hihhssh.
+SH hs hshhhshs S hihhhshh ss SS hihshshh HS ss hshshhss s hsshshs!
+SH s sh hshh SH SS hssshhss S hihhshhh sh S Hssshhss SS SS hishhhss s Hshshhhs s hs hsss hs SS Hihssssh S hihshssh s Hssssss.
+hs HS hihhsss!
+S SH Hihs S hishhhh!
+SS SH hisshhhh ss S Hshhhhss S Hssshhhs SH SS hshhsshs sh S hs Hssh s Hihhhhhs HS ss hihssshs sh HS Hisshss!
+s hshshhhh SH S HS Hshh SS sh hssssssh s Hsshsshh ss s hsshsssh SS SS Hshshhsh s Hshshhss s ss hih!
+SH hs hihhhhhs S hssshshs S Hsssshs!
+HS ss Hihshss.
+S hishhshs s Hihssshh sh hs hishhhhh s SS hsss s Hshhhsh!
+sh ss Hihhshss hs S Hshhshhs S hihhhshh sh HS hishssss SH S SH hiss S hshhhshs HS HS hssshhss ss SS Hsshshs!
+s Hssssssh SS s sh hshh SH ss Hshhsssh s hsshhshs hs S Hsshsshh HS SS hshhhsh!
+S hsshhssh S HS hihs hs SS hshshssh hss!
+Hssh Hsshs hssshhs.
+Hssshhhh Hihhhhhh!
+hshh Hshhs hissshh.
+Hshhhsh hihhshssh hiss hshhs Hihshhss Hihshs Hsshhhhh.
+Hish hssh.
+Hihssshh hishh hsshhsh.
+Hsss Hihs!
+Hshhhhss hihsh Hshhhsss hish hihhhh Hihshsshh Hssh.
+Hshshhsh Hish Hisshhs Hihhssss!
+hish!
+Hihhhsss hshs hihhssh.
+hishshssh hshsh hsshhshs Hshh Hihs Hsss!
+Hisshhsh Hishhhhh hsshhhhsh hihh hishh hihhshhs hshsssh Hihhshhhs hish hihs.
+hishhhhh Hshhhh hsshshhs!
+hiss hshhs hssshhhh hshhs hishshss hshs Hihshh hshssshh!
+hsshs Hshshsss hss.
+Hihsssh hsssshhhs Hishh hihsshhs hish hihhshhs Hishhsssh hssh!
+Hshhhhs!
+hihs hihs hshsh Hshshssh Hsh!
+hsss hsssh hsshshsh hshhssh.
+Hshshhsh.
+hssh Hihs.
+Hishhshh hsshssh hihhhhshs hshs Hshhs Hsshhhhh hisshs Hshsshsss hiss Hishs hsssshhs Hihhh hssshssh hssh hssss!
+hihhhshhh hssss hshshhss Hssh hshhhhh Hihhshhhh hssss hshhssh!
+Hish Hishshsh hshshhhhh Hishh Hshsshhh hihhshs Hsssshssh hsssshs Hihhshhhs hihs!
+hshsshhs hshsshs sh Hs!
+hisshh!
+Hsshhsssh hshsh hsshshhh Hshsss hihsshhs!
+Hsshshh Hshshsss.
+hisss Hshhshh!
+Hsshs!
+SS hss hshsshh Hihhhhhhs hihss hsshshhs hihsh Hsshsssh hisshhs hishhssss hihsh Hissshhs Hihh!
+hsshshsh hissssh Hsshhsssh Hihshs ss Hs.
+Hihss hisshhsh hsshshs Hihhhsshh hssssh hishhshhs Hihhh Hihsssss Hihhhhh Hissssshs Hihshh!
+SS hsh Hihsh Hishshhs Hshhsh!
+Hshhssshh hssshss hsshhhshs hihsh hihssshh Hshsshs HS Hsh hihhsh!
+hshsshssh hihhh Hihsshh!
+hishhs Hsshhshhh Hihsshs hsshssss.
+hshhs hshhsssh hihsss sh his hihhhh!
+Hishssshs Hihss hihsshhh hshsh Hihshhhh hishhsh Hishhhsh!
+Hsshss HS hss hihh!
+hsssssh.
+hshshsh hihhshshs hshsss hshhhhhs!
+Hihss Hihhhhsh hsssshh Hihhhsssh hshhsss hs Hs!
+Hsshh Hssshsh.
+Hishsss hisssshss hsssshh hshhshsss hissh hishshhh hisshss Hsshhhshs hihhshh hishssssh hshh.
+hihsshsh hishhs.
+HS His hsshshs hihhhhsh!
+Hihsh hihhhssh Hsshs.
+hisshssss Hshshh.
+hishshhhh hshhs hshhhhsh hihhss hs Hss hsshhhh hshssssss hssss Hisssshs Hshsh hshshhhh hihssh!
+Hisshhhhs hshhs!
+sh Hss hsshh Hssshhs.
+hshshs!
+hishsssss hihsss Hihhhhshs hsss.
+Hsssssh.
+Hishshs Hishhhhhh hshshhs ss Hss Hihs.
+hsssssh.
+Hsshhhh His sh hshhshs Hi!
+sh hssss Hihhsshh Hsshss hihhshhsh Hihshs!
+hss SH Hihhh Hshssssh hishhs Hshh!
+Hissss.
+hsh SH Hihss hshshhss Hisssh His ss hihhsss hsh SS hshs!
+Hisssssh Hsssh hsssshsh hshshh!
+His ss hishh hihssshh Hihhs Hissshs.
+Hisssss Hih SH Hihshh Hsh HS hsshh Hihshhhs Hishshh Hss sh Hisssh Hsssh Hshhh Hsshsss!
+hihsss!
+Hih sh hihhhs Hssshhhhs Hsss!
+hishhhh.
+hshsshh His HS hshhhhh Hi.
+ss hssss Hshhshs!
+hshhh.
+Hissshhsh Hsshshh His SS Hssh!
+Hihhhhh.
+hishss Hshss hisssss Hi!
+hs hsssh Hshhhshh Hisssh Hih ss hihssh.
+Hih SH hihhs hsshhhhs Hihsh hshhsssh Hisssss hs!
+ss Hissss hss SS hshsh Hihsshss Hsshshs Hih sh Hihhss hsss.
+Hsshh hisshhhh Hshshsh Hss SS Hihsss hihhshhss Hihhh hisshhss Hshhhss his SS hisshss Hih SH hsssh hssssshs Hsssss.
+His ss hssshss His ss hshss hshhhssh Hisshs Hishshhsh hihshss Hsh ss hishh hisshhhh hihss!
+Hshsh hishshs Hss ss Hissh hihshhhh Hisshs hss ss hishhh.
+hsh HS Hihsh hsshhss.
+hihhs hisshssh hssshsh his SS hisss!
+hsh SH Hiss!
+hisssshh hisshss hsh HS hshhs!
+Hssss hsssh Hshhhshs Hsshhs!
+Hih SH hisshh Hisshshss Hsshs Hishhhs!
+hsss Hiss hihh His.
+hss!
+hss.
+hssh Hsh!
+Hssh Hss!
+Hish hshh hihs hiss hssh Hih.
+hiss hih!
+hssh Hiss hsss hshh Hiss hshh"""
+
+def unhiss_palette(hiss):
+    """Decipher a standard VGA palette from weasel hisses"""
+    dec = hiss.lower().replace("\n", " ").split(" ")
+
+    out = []
+    pr = []
+    while dec:
+        nxt = dec.pop(0)
+        l = len(nxt)
+        if l == 4:
+            out.append(0)
+            continue
+        elif l == 1:
+            pr.append(5)
+        elif l > 4:
+            pr.append(9-l)
+        else:
+            pxt = dec.pop(0)
+            k = len(pxt) - 1
+            pr.append((6 if l == 2 else 11) + k)
+        if len(pr) == 2:
+            out.append(int(f"{hex(pr[0])[2:]}{hex(pr[1])[2:]}", 16))
+            pr = []
+    return out
+
+print(unhiss_palette(hisses))
